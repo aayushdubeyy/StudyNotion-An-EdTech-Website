05647e04c15479320c73fe26bbd6358a4cfb02e0
aayushdubeyy <aayushdubey040@gmail.com>
2024-02-12 03:01:26 +0530

    fourth commit

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://study-notion-an-ed-tech-website.vercel.app",
         credentials: true,
     })
 )
